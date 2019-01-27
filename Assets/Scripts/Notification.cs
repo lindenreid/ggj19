cfda0677b838e2c3eaaedd5acf5e562a249c356f
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2019-01-26 19:17:15 -0800

    added accuracy notification animation

---
+++ Assets/Scripts/Notification.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Notification : MonoBehaviour
+{
+    public Vector3 originalLocation;
+    public Vector3 anim;
+    public float duration;
+
+    private float timeRemaining;
+
+    void Start () {
+        Restart(enable:false);
+    }
+
+    void Update () {
+        timeRemaining -= Time.deltaTime;
+        if(timeRemaining > 0.0f) {
+            transform.localPosition += anim * Time.deltaTime;
+        } else {
+            Restart(enable:false);
+        }
+    }
+
+    public void Restart(bool enable) {
+        transform.localPosition = originalLocation;
+        timeRemaining = duration;
+        gameObject.SetActive(enable);
+    }
+}
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -3,9 +3,9 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-    public GameObject missObj;
-    public GameObject okObj;
-    public GameObject perfObj;
+    public Notification missNotif;
+    public Notification okNotif;
+    public Notification perfNotif;
     public GameObject comboObj;
 
     public Text ScoreText;
@@ -21,19 +21,19 @@ public class UI : MonoBehaviour
     public void ShowAccuracy(Accuracy acc) {
         switch(acc) {
             case Accuracy.Great:
-                missObj.SetActive(false);
-                okObj.SetActive(false);
-                perfObj.SetActive(true);
+                missNotif.Restart(enable:false);
+                okNotif.Restart(enable:false);
+                perfNotif.Restart(enable:true);
             break;
             case Accuracy.Ok:
-                missObj.SetActive(false);
-                okObj.SetActive(true);
-                perfObj.SetActive(false);
+                missNotif.Restart(enable:false);
+                okNotif.Restart(enable:true);
+                perfNotif.Restart(enable:false);
             break;
             case Accuracy.Miss:
-                missObj.SetActive(true);
-                okObj.SetActive(false);
-                perfObj.SetActive(false);
+                missNotif.Restart(enable:true);
+                okNotif.Restart(enable:false);
+                perfNotif.Restart(enable:false);
             break;
             default:
                 Debug.LogError("accuracy case not handled!");
