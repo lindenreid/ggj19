c558b408fc560b26c4866eee53d2e8aa1f963fca
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2019-01-27 12:17:17 -0800

    made dumpling jump when finished

--- Assets/Scripts/DumplingAnimator.cs
+++ Assets/Scripts/DumplingAnimator.cs
@@ -2,11 +2,18 @@
 
 public class DumplingAnimator : MonoBehaviour
 {
+    public SpriteRenderer SpriteRenderer;
     public Animator anim;
     public string animName;
     public int numFrames;
+    public Vector3 originalLocation;
+    public Vector3 finishAnimation;
+    public float fadeSpeed;
+    public float finishAnimDuration;
 
     private int frame;
+    private float animLeft;
+    private bool playingFinishAnimation;
 
     void Start()
     {
@@ -14,15 +21,64 @@ public class DumplingAnimator : MonoBehaviour
         anim.speed = 0.0f;
 
         frame = 0;
+        animLeft = 0.0f;
+    }
+
+    void Update () {
+        if(playingFinishAnimation) {
+            animLeft -= Time.deltaTime;
+            transform.localPosition += finishAnimation * Time.deltaTime;
+            float a = SpriteRenderer.color.a - fadeSpeed * Time.deltaTime;
+            SpriteRenderer.color = new Color(
+                SpriteRenderer.color.r,
+                SpriteRenderer.color.b,
+                SpriteRenderer.color.g,
+                a
+            );
+            if(animLeft <= 0.0f) {
+                EndAnimation();
+                frame = 0;
+            }       
+        }
     }
 
     public void IncrementFrame() {
-        frame++;
-        if(frame > numFrames) {
+        if(!playingFinishAnimation) {
+            frame++;
+        }
+        else {
+            EndAnimation();
             frame = 0;
         }
 
-        anim.Play(animName, -1, (float)frame / (float)numFrames);
+        if(frame == numFrames) {
+            StartAnimation();
+            anim.Play(animName, -1, 0.9f);
+        } else {
+            anim.Play(animName, -1, (float)frame / (float)numFrames);
+        }
+
         anim.speed = 0.0f;
+    }
+
+    private void EndAnimation () {
+        playingFinishAnimation = false;
+        transform.localPosition = originalLocation;
+        anim.Play(animName, -1, 0);
+        anim.speed = 0.0f;
+
+        SpriteRenderer.color = new Color(
+            SpriteRenderer.color.r,
+            SpriteRenderer.color.b,
+            SpriteRenderer.color.g,
+            1.0f
+        );
+    }
+
+    private void StartAnimation () {
+        animLeft = finishAnimDuration;
+        playingFinishAnimation = true;
+        //transform.localPosition += finishAnimation * Time.deltaTime;
+        //animLeft -= Time.deltaTime;
     }
 }
